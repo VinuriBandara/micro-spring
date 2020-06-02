658cc100cdcd11d2e1f5c1059af1cd2788ecd0a8
VinuriBandara <vinugayanthika@gmail.com>
2020-06-02 18:03:46 +0530

    Eureka Nameserver completed

--- forex-service/src/main/resources/data.sql
+++ forex-service/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
-values(10001,'USD','LKR',65,0);
+values(10001,'USD','LKR',185,0);
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
 values(10002,'EUR','INR',75,0);
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
